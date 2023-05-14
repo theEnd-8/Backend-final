befee1fe639f971aaa7eff32f39357303f6412f4
theEnd-8 <120683093+theEnd-8@users.noreply.github.com>
2023-05-14 19:08:59 +0600

    Backend-final
    Backend-final

---
+++ products/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+from products.models import ProductCategory, Product
+
+
+def index(request):
+    context = {
+        'title': 'Store - Main'
+    }
+    return render(request, 'products/index.html', context)
+
+
+def products(request):
+    context = {
+        'title': 'Store - Каталог',
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
+    }
+    return render(request, 'products/products.html', context)
